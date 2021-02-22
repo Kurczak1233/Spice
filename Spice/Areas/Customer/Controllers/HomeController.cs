55653b3032981dfd718cdc1d8b6c0140629f612f
Kurczak1233 <kurczak12333@gmail.com>
2021-02-22 17:58:02 +0100

    Setting up the session on login and logout and in home controller

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -37,6 +37,16 @@ namespace Spice.Controllers
                 CouponList = await _db.Coupon.Where(c=>c.IsActive == true).ToListAsync() //Tylko aktywne
 
             };
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim!=null)
+            {
+                var cnt = _db.ShoppingCart.Where(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32("ssCartCount", cnt);
+            }
+
             return View(IndexVM);
         }
 
