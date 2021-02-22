fa54a9116d0794dd4349030c2fb8f32135f4d54c
Kurczak1233 <kurczak12333@gmail.com>
2021-02-22 19:09:36 +0100

    Create ViewModel (in order to create props in view)

---
+++ Spice/Models/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModels
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> listCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
