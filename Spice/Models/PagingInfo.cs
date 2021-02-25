81d3db36641aaee09f7c7c5d8b65ca632b2b934d
Kurczak1233 <kurczak12333@gmail.com>
2021-02-25 10:13:31 +0100

    Create model for pagination

---
+++ Spice/Models/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+        public int ItemsForPage{ get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsForPage);
+        public string urlParam { get; set; } //For storing the page number
+    }
+}
