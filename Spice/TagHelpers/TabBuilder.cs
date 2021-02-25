a19e67b2e4cf701634917bd1635b2abc99b4dab2
Kurczak1233 <kurczak12333@gmail.com>
2021-02-25 10:33:49 +0100

    Create custom tag helper

---
+++ Spice/TagHelpers/TabBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Spice.TagHelpers
+{
+    internal class TabBuilder
+    {
+        private string v;
+
+        public TabBuilder(string v)
+        {
+            this.v = v;
+        }
+    }
+}
